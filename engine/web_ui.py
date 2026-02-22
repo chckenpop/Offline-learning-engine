00296767151fb5587d007acbc563f447a7720e1e
huzaifa024 <zombarkarhuzaifa024@gmail.com>
2026-02-22 13:15:49 +0530

    Final deployment fixes

--- engine/web_ui.py
+++ engine/web_ui.py
@@ -604,4 +604,5 @@ def run_server(port=8000):
     except KeyboardInterrupt: server.server_close()
 
 if __name__ == '__main__':
-    run_server(8000)
+    port = int(os.environ.get('PORT', 8000))
+    run_server(port)
